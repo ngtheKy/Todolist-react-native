2b606f690e1d907dbb7c7955bd24310b852efc7c
ngtheKy <67519435+ngtheKy@users.noreply.github.com>
2021-03-26 09:55:45 +0700

    addTodos

---
+++ task.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Task = (props) => {
+
+    return (
+        <View>
+            <Text>
+                {props.text}
+            </Text>
+        </View>
+    )
+}
+
+export default Task
